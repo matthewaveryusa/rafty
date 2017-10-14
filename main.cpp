66bfb654b2ee74d5b2986fa3c7189f90c2e5c2ea
Matthew Avery <matthewaveryusa@gmail.com>
2017-10-13 23:43:31 -0400

    peerup

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@
 #include <rafty.pb.h>
 #include <gflags/gflags.h>
 
+namespace msg {
+  constexpr char end_peer = 'A';
+  constexpr char start_peer_bind = 'B';
+  constexpr char start_peer_connect = 'C';
+  constexpr char peer_up = 'U';
+  constexpr char to_all_peers = 'P';
+}
+
 void storage(lmdb::env &env) {
   struct nn_pollfd pfd[1];
 
@@ -24,9 +32,9 @@ void storage(lmdb::env &env) {
   nn::socket pipeline2_s(AF_SP, NN_PUSH);
   pipeline2_s.connect("inproc://to_brain");
 
-  auto wtxn = lmdb::txn::begin(env);
-  auto keydb = lmdb::dbi::open(wtxn, "keys", MDB_CREATE);
-  wtxn.commit();
+  auto txn = lmdb::txn::begin(env);
+  auto keydb = lmdb::dbi::open(txn, "keys", MDB_CREATE);
+  txn.commit();
   while(1) {
     int rc = nn_poll (pfd, 1, 60000);
     if (rc == 0) {
@@ -46,9 +54,9 @@ void storage(lmdb::env &env) {
         if(!val.ParseFromArray(buf, msgsize)) {
           printf ("storage parse failure!\n");
         }
-        wtxn = lmdb::txn::begin(env);
-        keydb.put(wtxn,val.index(), lmdb::val{buf, (size_t)msgsize});
-        wtxn.commit();
+        txn = lmdb::txn::begin(env);
+        keydb.put(txn,val.index(), lmdb::val{buf, (size_t)msgsize});
+        txn.commit();
         nn_freemsg(buf);
       }
     }
@@ -56,9 +64,18 @@ void storage(lmdb::env &env) {
 }
 
 void brain(lmdb::env &env) {
-  auto wtxn = lmdb::txn::begin(env);
-  auto statedb = lmdb::dbi::open(wtxn, "state", MDB_CREATE);
-  wtxn.commit();
+  auto txn = lmdb::txn::begin(env);
+  auto statedb = lmdb::dbi::open(txn, "state", MDB_CREATE);
+  txn.commit();
+
+  txn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
+  auto cursor = lmdb::cursor::open(txn, statedb);
+  lmdb::val val;
+  cursor.get(lmdb::val{"config"}, val, MDB_NEXT);
+  rafty::Config config;
+  if(!config.ParseFromArray(val.data(), val.size())) {
+    printf ("brain config parse failure!\n");
+  }
 
   struct nn_pollfd pfd[1];
 
@@ -67,11 +84,26 @@ void brain(lmdb::env &env) {
 
   nn::socket pipeline_s(AF_SP, NN_PULL);
   pipeline_s.bind("inproc://to_brain");
+  
+  //fire up peers that are in config file
+  for(int i = 0; i < config.peers_size(); ++i) {
+    std::ostringstream oss;
+    if(config.peers(i) < config.listen()) {
+      oss << msg::start_peer_connect << config.peers(i);
+    } else {
+      oss << msg::start_peer_bind << config.peers(i);
+    }
+    auto msg = oss.str();
+    pub_s.send(msg.c_str(), msg.size(), 0);
+  }
+
+
   pfd[0].fd = pipeline_s.fd();
   pfd[0].events = NN_POLLIN;
+  bool is_leader = true;
   
   while(1) {
-    int rc = nn_poll (pfd, 1, 60000);
+    int rc = nn_poll (pfd, 1, config.timeout_ms());
     if (rc == 0) {
       printf ("brain idling!\n");
       continue;
@@ -84,7 +116,15 @@ void brain(lmdb::env &env) {
       char* buf = nullptr;
       int msgsize = pipeline_s.recv(&buf, NN_MSG, 0);
       if(msgsize >= 0) {
-        printf ("brain Message received! %.*s\n", msgsize, buf);
+        if(buf[0] == msg::peer_up) {
+          printf ("peerup! %.*s\n", msgsize, buf);
+          if(is_leader) {
+            std::ostringstream oss;
+            oss << msg::to_all_peers << "is_leader";
+            auto msg = oss.str();
+            pub_s.send(msg.c_str(), msg.size(), 0);
+          }
+        }
         nn_freemsg(buf);
       }
     }
@@ -101,6 +141,7 @@ void clients() {
 
   nn::socket sub_s(AF_SP, NN_SUB);
   sub_s.connect("inproc://from_brain");
+  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, &msg::to_all_peers, 1);
   pfd[1].fd = sub_s.fd();
   pfd[1].events = NN_POLLIN;
   
@@ -126,24 +167,33 @@ void clients() {
       }
     }
     if (pfd[1].revents & NN_POLLIN) {
-      printf ("clients Message can be received from brain socket!\n");
+      char* buf = nullptr;
+      int msgsize = sub_s.recv(&buf, NN_MSG, 0);
+      printf ("clients Message can be received from brain socket %.*s!\n", msgsize, buf);
+      nn_freemsg(buf);
     }
   }
 }
 
-void peer(std::string address, nn::socket &s) {
+void peer(std::string address, nn::socket s) {
   struct nn_pollfd pfd[2];
 
   pfd[0].fd = s.fd();
   pfd[0].events = NN_POLLIN;
 
   nn::socket sub_s(AF_SP, NN_SUB);
+  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, &msg::to_all_peers, 1);
   sub_s.connect("inproc://from_brain");
   pfd[1].fd = sub_s.fd();
   pfd[1].events = NN_POLLIN;
 
   nn::socket pipeline_s(AF_SP, NN_PUSH);
   pipeline_s.connect("inproc://to_brain");
+ 
+  std::ostringstream oss;
+  oss << msg::peer_up << address;
+  auto msg = oss.str();
+  pipeline_s.send(msg.c_str(), msg.size(), 0);
 
   while(1) {
     int rc = nn_poll (pfd, 2, 60000);
@@ -159,7 +209,10 @@ void peer(std::string address, nn::socket &s) {
       printf ("peers Message can be received from peer socket!\n");
     }
     if (pfd[1].revents & NN_POLLIN) {
-      printf ("peers Message can be received from brain socket!\n");
+      char* buf = nullptr;
+      int msgsize = sub_s.recv(&buf, NN_MSG, 0);
+      printf ("peers Message can be received from brain socket %.*s!\n", msgsize, buf);
+      nn_freemsg(buf);
     }
   }
 }
@@ -200,21 +253,13 @@ int main(int argc, char** argv) {
     std::string serialized_config;
     config.SerializeToString(&serialized_config);
 
-    auto wtxn = lmdb::txn::begin(env);
-    auto statedb = lmdb::dbi::open(wtxn, "state", MDB_CREATE);
+    auto txn = lmdb::txn::begin(env);
+    auto statedb = lmdb::dbi::open(txn, "state", MDB_CREATE);
     lmdb::val key{"config"};
     lmdb::val value{serialized_config};
-    statedb.put(wtxn, key, value);
-    wtxn.commit();
-
-    auto txn = lmdb::txn::begin(env, nullptr, MDB_RDONLY);
-    auto cursor = lmdb::cursor::open(txn, statedb);
-    while (cursor.get(key, value, MDB_NEXT)) {
-      std::printf("key: '%.*s', value: '%zu'\n", (int) key.size(), key.data(), value.size());
-    }
-    cursor.close();
-    txn.abort();
-
+    statedb.put(txn, key, value);
+    txn.commit();
+    std::cout << "init done" << std::endl;
     return 0;
   }
 
@@ -227,12 +272,26 @@ int main(int argc, char** argv) {
 
   nn::socket sub_s(AF_SP, NN_SUB);
   sub_s.connect("inproc://from_brain");
-  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, "end_peer ", sizeof("end_peer")-1);
-  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, "start_peer ", sizeof("end_peer")-1);
+  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, &msg::end_peer, 1);
+  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, &msg::start_peer_bind, 1);
+  sub_s.setsockopt(NN_SUB, NN_SUB_SUBSCRIBE, &msg::start_peer_connect, 1);
   while(1) {
     char* buf = nullptr;
     int msgsize = sub_s.recv(&buf, NN_MSG, 0);
-    printf ("resource manaager message received! %.*s\n", msgsize, buf);
+    if(buf[0] == msg::start_peer_bind || buf[0] == msg::start_peer_connect) {
+      std::string p(&buf[1], msgsize-1);
+      std::cout << "starting peer " << p << std::endl;
+      nn::socket s(AF_SP, NN_PAIR);
+      if(buf[0] == msg::start_peer_bind) {
+        s.bind(p.c_str());
+      } else {
+        s.connect(p.c_str());
+      }
+      threads.emplace(":" + p, std::thread{peer, std::move(p), std::move(s)});
+    } else {
+      std::string peer(&buf[1], msgsize-1);
+      std::cout << "ending peer " << peer << std::endl;
+    }
     nn_freemsg(buf);
   }
   
